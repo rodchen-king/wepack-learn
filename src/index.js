ab96a29bd643b148043109589cbe643343b24015
rodchen <rodchen_king@outlook.com>
2020-09-09 10:15:18 +0800

    1. webpack4.44.1 似乎内部封装了babel-loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
+import module from './es6/es6Module';
 import './index.css';
 
 function component() {
   var element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], module.add(1, 2));
   element.classList.add('hello');
 
   return element;
